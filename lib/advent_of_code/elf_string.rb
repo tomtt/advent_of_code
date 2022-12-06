7dfca5f07f6a8acd77234faef2a02658ded918c3
Tom ten Thij <code@tomtenthij.nl>
2022-12-06 05:45:24 +0100

    Use String#ints

--- lib/advent_of_code/elf_string.rb
+++ lib/advent_of_code/elf_string.rb
@@ -4,6 +4,20 @@ module AdventOfCode
       ord > 96 ? ord - 96 : ord - 38
     end
 
+    def ints
+      scan(/-?\d+/).map(&:to_i)
+    end
+
+    def char_columns(offset:, step:, number:, width: 1)
+      chars = []
+      index = offset
+      number.times do
+        chars << self[index, width]
+        index += step
+      end
+      chars
+    end
+
     def present?
       !gsub(/\s/, "").empty?
     end
--- spec/advent_of_code/elf_string_spec.rb
+++ spec/advent_of_code/elf_string_spec.rb
@@ -16,4 +16,60 @@ RSpec.describe String do
       expect("Zx".elf_priority).to eq 52
     end
   end
+
+  describe "#ints" do
+    it "is empty array for a string with no digits" do
+      expect("".ints).to eq []
+    end
+
+    it "is an item with single number when there is one number" do
+      expect("1234".ints).to eq [1234]
+    end
+
+    it "is an item with each number when there is are multiple numbers" do
+      expect("12 34".ints).to eq [12, 34]
+    end
+
+    it "works with negative numbers" do
+      expect("-43 -21".ints).to eq [-43, -21]
+    end
+
+    it "treats any non number character as a separator" do
+      expect(" [1](2)-3- 4abcde5\n6!@#$%^&*{7.8}".ints).to eq [1, 2, -3, 4, 5, 6, 7, 8]
+    end
+
+    it "does not treat a lone dash as a number" do
+      expect(" - ".ints).to eq []
+    end
+
+    it "treats any dash in front of a number as a negative sign" do
+      expect("1 --2-3-4".ints).to eq [1, -2, -3, -4]
+    end
+  end
+
+  describe "#char_columns" do
+    it "returns the right single offset" do
+      expect("abcdef".char_columns(offset: 1, step: 1, number: 1)).to eq ["b"]
+      expect("abcdef".char_columns(offset: 3, step: 1, number: 1)).to eq ["d"]
+    end
+
+    it "returns the right number of characters" do
+      expect("abcdef".char_columns(offset: 1, step: 1, number: 3)).to eq ["b", "c", "d"]
+      expect("abcdef".char_columns(offset: 3, step: 1, number: 3)).to eq ["d", "e", "f"]
+    end
+
+    it "makes the right steps" do
+      expect("abcdefghi".char_columns(offset: 1, step: 2, number: 3)).to eq ["b", "d", "f"]
+      expect("abcdefghi".char_columns(offset: 3, step: 2, number: 3)).to eq ["d", "f", "h"]
+    end
+
+    it "puts nil where no character was present" do
+      expect("abcde".char_columns(offset: 2, step: 2, number: 3)).to eq ["c", "e", nil]
+    end
+
+    it "can provide multi char columns" do
+      result = "abcdefgh".char_columns(offset: 1, step: 3, number: 3, width: 2)
+      expect(result).to eq ["bc", "ef", "h"]
+    end
+  end
 end
